75fb6cc8d6d3cc963ff644198887656fcec04417
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-06-06 06:46:54 +0200

    Add the delete button 'X' functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function newItem(){
+
 //1. Adding a new item to the list of items:
 
     let list = $('#list');
@@ -12,13 +13,24 @@ function newItem(){
         list.append(li);
     }
  
-//   //2. Crossing out an item from the list of items:
+//2. Crossing out an item from the list of items:
  
     li.on("dblclick", function() {
         li.addClass("strike");
       });
  
-//   //3(i). Adding the delete button "X": 
+//3(i). Adding the delete button "X": 
+
+    let crossOutButton = $('<crossOutButton>');
+        crossOutButton.append(document.createTextNode('X'));
+        li.append(crossOutButton);
+
+        crossOutButton.on("click", function() {
+            li.addClass('delete');
+            li.append('.delete');
+        });
+
+
 //     let crossOutButton = document.createElement("crossOutButton");
 //       crossOutButton.appendChild(document.createTextNode("X"));
 //       li.appendChild(crossOutButton);
