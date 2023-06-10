d85d1dd7de5cdafddfae02490789242fbbb1991c
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-06-10 13:03:30 +0200

    Update JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,38 @@ function newItem(){
 
 //1. Adding a new item to the list of items:
 
-    let list = $('#list');
     let li = $('<li></li>');
     let inputValue = $('#input').val();
-    li.append(inputValue);  
+    li.append(inputValue);
 
     if (inputValue === '') {
-        alert('You must write something!');
+    alert("You must write something!");
     } else {
-        list.append(li);
+    $('#list').append(li);
     }
+
  
 //2. Crossing out an item from the list of items:
- 
-    li.on("dblclick", function() {
-        li.addClass("strike");
-      });
+
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
+    
  
 //3(i). Adding the delete button "X": 
 
-    let crossOutButton = $('<crossOutButton>');
-        crossOutButton.append(document.createTextNode('X'));
-        li.append(crossOutButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
-        crossOutButton.on("click", function() {
-            li.addClass('delete');
-            li.append('.delete');
-        });
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem(){
+		li.addClass("delete")
+	}
 
 //   // 4. Reordering the items: 
 
